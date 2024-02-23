94ed63ff92ebb4b94356528f5be738e0571978ba
Lanscarlos <lanscarlos@hotmail.com>
2024-02-23 20:36:34 +0800

    backup

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -3,8 +3,19 @@ import { createStore } from 'vuex';
 export default createStore({
     state() {
         return {
-            user: null,
-            profile: null,
+            // user: null,
+            // profile: null,
+            user: {
+                email: 'lanscarlos@hotmail.com',
+                password: '123456'
+            },
+            profile: {
+                username: '卡洛',
+                signature: '这个人很懒什么也没留下',
+                avatar: 'https://avatars.githubusercontent.com/u/32390930?v=4',
+                gender: -1,
+                birthday: -1
+            },
             cart: {
                 id: 0,
                 items: [],
